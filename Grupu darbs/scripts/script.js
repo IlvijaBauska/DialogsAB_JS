f522b7362f8f18aa4bd152ecf6b522fcc9efe6bd
IlvijaBauska <Ilvija.bauska@inbox.lv>
2024-11-20 20:49:27 +0200

    Grupu darbs

---
+++ Grupu darbs/scripts/script.js
@@ -0,0 +1,22 @@
+<script>collasuzcm();</script> 
+
+    <script> function collasuzcm() {let c = document.getElementById("collas").value; let cm = c * 2.54; 
+
+  
+let izvade = "Rezultāts: " + c + " collas ir " + cm + " centimetri!"; 
+
+ return izvade;} 
+
+</script> 
+
+  <p style="color:white; 
+
+ font-family:Arial;" id="Footeris"></p> 
+
+<script> 
+ 
+    const Footeritis = {Vaaards: "Ilvija", Uzvaardsss:"Bauska", Datums: "11/20/2024"}; 
+
+   document.getElementById("Footeris").innerHTML = Footeritis.Vaaards + " " + Footeritis.Uzvaardsss + " | " + Footeritis.Datums 
+
+</script> 
