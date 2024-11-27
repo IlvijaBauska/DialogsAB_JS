a9be10f878c02527fe3689c451966fc73cce66be
IlvijaBauska <Ilvija.bauska@inbox.lv>
2024-11-27 20:41:22 +0200

    Class_11a

--- Class_10/scripts/script2.js
+++ Class_10/scripts/script2.js
@@ -1,8 +1,25 @@
+//https://www.w3schools.com/js/tryit.asp?filename=tryjs_object_constructor
+
 function Person(first, last, age, eye) {
     this.firstName = first;
     this.lastName = last;
     this.age = age;
     this.eyeColor = eye;
-  }
-  const myFather = new Person("John", "Doe", 50, "blue");
-  const myMother = new Person("Anna", "Lala", 30, "green");
+    this.city = "Rīga"
+    this.nationality = "Latvian"
+    this.fullName = function() {
+        return this.firstName + " " + this.lastName
+      };
+    }
+
+//const myFather = {firstName: "John", latsName: "Doe", age: 50, eye:"blue"};
+const myFather = new Person("John", "Doe", 50, "blue");
+
+//const myFather = {firstName: "Anna", latsName: "Hill", age: 48, eye:"green"};
+const myMother = new Person("Anna", "Hill", 48, "green");
+
+const myBrother = new Person("Martin", "OLapa", 20, "blue");
+
+document.getElementById("demo").innerHTML = "My father is " + myFather.fullName();
+
+//document.getElementById("demo").innerHTML = "My mother is " + myMother.fullName();
