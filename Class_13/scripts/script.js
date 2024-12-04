7ca065bfd63dd59e50bdf816ab895fe42e9036cc
IlvijaBauska <Ilvija.bauska@inbox.lv>
2024-12-04 18:57:58 +0200

    Class_13

---
+++ Class_13/scripts/script.js
@@ -0,0 +1,15 @@
+// https://www.javascripttutorial.net/javascript-dom/javascript-getelementsbytagname/
+// https://www.javascripttutorial.net/javascript-dom/javascript-queryselector/
+
+let btn = document.getElementById('btnCount');
+
+
+btn.addEventListener('click', () => {
+    let headings = document.getElementsByTagName('h2');
+    alert(`The number of H2 tags (counted by getElementsByTagName): ${headings.length}`);
+});
+
+btn.addEventListener('dblclick', () => {
+    let headings = document.querySelectorAll('h2');
+    alert(`The number of H2 tags (counted by querySelectorAll): ${headings.length}`);
+});
